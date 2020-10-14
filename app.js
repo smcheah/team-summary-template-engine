3c86b6942da6f77af9c00c98e9dba4ad1feccf62
Simone Cheah <simone_cheah@hotmail.com>
2020-10-15 03:35:12 +1100

    ✨ added initial prompt messages and role questions

--- app.js
+++ app.js
@@ -10,17 +10,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const whichRole = [
+	{
+		type: "checkbox",
+		name: "roles",
+		message: "which of the roles below are in your team?",
+		choices: [
+			{
+				name: "Manager",
+				value: "manager",
+			},
+			{
+				name: "Engineer",
+				value: "engineer",
+			},
+			{
+				name: "Intern",
+				value: "intern",
+			},
+		],
+	},
+];
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Name:",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "ID:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Email:",
+    },
+];
+
+function roleQuestion(role) {
+	if (role === "manager") {
+		questions.push({
+			type: "input",
+			name: "officeNumber",
+			message: "Office Number:",
+		});
+	}
+	if (role === "engineer") {
+		questions.push({
+			type: "input",
+			name: "github",
+			message: "Github Username:",
+		});
+	}
+	if (role === "intern") {
+		questions.push({
+			type: "input",
+			name: "school",
+			message: "School:",
+		});
+	}
+}
+
+async function init() {
+	console.log(`
+--------------------------------------------------------
+TEAM SUMMARY TEMPLATE ENGINE
+
+Answer the prompts to create your own team summary page!
+--------------------------------------------------------`);
+
+	try {
+		const { roles } = await inquirer.prompt(whichRole);
+        console.log(roles);
+
+        roles.forEach(role => {
+            await inquirer.prompt(questions);
+            await inquirer.prompt(roleQuestion(role));
+        })
+
+	} catch (err) {
+		console.log(`Error: ${err}`);
+	}
+}
+init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+async function renderHTML() {}
+render();
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
+
+
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
